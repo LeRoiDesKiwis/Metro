b221273bb150f2a76161543b43ace86f14d77bdd
Fan2lasagne <150160586+Fan2lasagne@users.noreply.github.com>
2023-11-14 14:26:03 +0100

    Player now extends GameCharacter

--- src/fr/metro/characters/GameCharacter.java
+++ src/fr/metro/characters/GameCharacter.java
@@ -1,7 +1,13 @@
 package fr.metro.characters;
 
 public abstract class GameCharacter {
+    private int hp;
+    private final String name;
+    private final Inventory inventory;
 
-    //TODO
-
+    protected GameCharacter(int hp, String name, Inventory inventory) {
+        this.hp = hp;
+        this.name = name;
+        this.inventory = inventory;
+    }
 }
--- src/fr/metro/characters/Player.java
+++ src/fr/metro/characters/Player.java
@@ -1,15 +1,8 @@
 package fr.metro.characters;
 
-public class Player /*extends GameCharacter ??*/{
+public class Player extends GameCharacter {
 
-    private int hp;
-    private final String name;
-
-    private final Inventory inventory;
-
-    public Player(int hp, String name, Inventory inventory) {
-        this.hp = hp;
-        this.name = name;
-        this.inventory = inventory;
+    public Player(int hp, String name) {
+        super(hp, name, new Inventory());
     }
 }
