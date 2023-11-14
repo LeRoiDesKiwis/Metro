76ad7a98915e349de238426cc98f011e31631260
PaulCB25 <76494231+PaulCB25@users.noreply.github.com>
2023-11-14 15:38:30 +0100

    Added armor class

--- src/fr/metro/characters/Inventory.java
+++ src/fr/metro/characters/Inventory.java
@@ -7,5 +7,8 @@ import java.util.List;
 
 public class Inventory {
     private final List<Item> items = new ArrayList<>();
-    private final Item[] equipment = new Item[4]; // 0 head, 1 torso, 2 legs, 3 feet
+    private final Item[] equipment = new Item[4]; // 0 = head, 1 = torso, 2 = legs, 3 = feet
+
+    public Inventory(){
+    }
 }
---
+++ src/fr/metro/items/Armor.java
@@ -0,0 +1,8 @@
+package fr.metro.items;
+
+public abstract class Armor extends Item{
+
+    public Armor(String name, String description){
+        super(name, description);
+    }
+}
