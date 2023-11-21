3e9e6db67694f455bb1e67a3f3a2fae2f20a390b
LeRoiDesKiwis <ferryantony1@gmail.com>
2023-11-21 15:12:27 +0100

    Move key

--- src/fr/metro/items/Key.java
+++ src/fr/metro/items/Key.java
@@ -1,8 +1,7 @@
-package fr.metro.characters.items;
+package fr.metro.items;
 import fr.metro.items.Item;
 
 public class Key extends Item {
-
     public Key(String name, String description) {
         super(name, description);
     }
