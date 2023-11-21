2ea0821d1910e4ea4fe09b1ded27f60066d8d782
PaulCB25 <76494231+PaulCB25@users.noreply.github.com>
2023-11-21 15:11:00 +0100

    Tried to add merchant and money

---
+++ src/fr/metro/characters/friends/Merchant.java
@@ -0,0 +1,12 @@
+package fr.metro.characters.friends;
+
+import fr.metro.characters.Inventory;
+
+public class Merchant extends FriendlyCharacter{
+
+    public Merchant(String name, int hp, Inventory inventory, String introLine){
+        super(name, hp, inventory, introLine);
+    }
+
+    
+}
