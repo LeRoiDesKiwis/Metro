6ddc7e4457045b7702dcdacf0cf801c28296ca1c
Fan2lasagne <150160586+Fan2lasagne@users.noreply.github.com>
2023-11-21 15:50:11 +0100

    Added Food and heal

---
+++ src/fr/metro/items/foods/CannedFood.java
@@ -0,0 +1,8 @@
+package fr.metro.items.foods;
+
+public class CannedFood extends Food{
+
+    public CannedFood(){
+        super("Canned food", "It's a little rusty, but the food must be fine", 10);
+    }
+}
--- src/fr/metro/items/weapons/Knife.java
+++ src/fr/metro/items/weapons/Knife.java
@@ -1,7 +1,5 @@
 package fr.metro.items.weapons;
 
-import fr.metro.items.Weapon;
-
 public class Knife extends Weapon {
     public Knife(){
         super("Knife","It's a knife, maybe useful to cut butter.",4);
