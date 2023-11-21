c720541e82344afc8e426fc5db1e92d8ddc5f043
Fan2lasagne <150160586+Fan2lasagne@users.noreply.github.com>
2023-11-21 14:44:06 +0100

    Added Nonplayer

---
+++ src/fr/metro/characters/Nonplayer.java
@@ -0,0 +1,8 @@
+package fr.metro.characters;
+
+public class Nonplayer extends GameCharacter{
+
+    public Nonplayer(String name, int hp, Inventory inventory){
+        super(name, hp, inventory);
+    }
+}
