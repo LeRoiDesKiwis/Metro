877f20ec49f44710d78016379fdd93b26a60806b
LeRoiDesKiwis <lekiwidu17@gmail.com>
2023-11-21 19:07:27 +0100

    Add tick() to Game

--- src/fr/metro/game/Game.java
+++ src/fr/metro/game/Game.java
@@ -1,14 +1,27 @@
 package fr.metro.game;
 
 import fr.metro.characters.Player;
+import fr.metro.commands.manager.CommandManager;
+
+import java.util.Scanner;
 
 public class Game {
 
     private final Player player;
     private final Board board;
+    private final CommandManager commandManager;
 
     public Game(Player player, Board board) {
         this.player = player;
         this.board = board;
+        this.commandManager = new CommandManager(player);
+    }
+
+    public void tick(){
+        Scanner scanner = new Scanner(System.in);
+        while(!commandManager.execute(scanner.nextLine())){
+            System.out.println();
+            System.out.print("La commande a échoué, réessaye : ");
+        }
     }
 }
