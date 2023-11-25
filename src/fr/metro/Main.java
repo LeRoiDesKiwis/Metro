0f99d5d2772e0877ad5afcc5687cb5faf3f69f8b
LeRoiDesKiwis <ferryantony1@gmail.com>
2023-11-25 16:00:03 +0100

    Auto-detect name in args

--- src/fr/metro/Main.java
+++ src/fr/metro/Main.java
@@ -2,27 +2,31 @@ package fr.metro;
 
 import fr.metro.characters.Inventory;
 import fr.metro.characters.Player;
-import fr.metro.characters.friends.FriendlyCharacter;
 import fr.metro.game.Board;
 import fr.metro.game.Exit;
 import fr.metro.game.Game;
 import fr.metro.game.Location;
 import fr.metro.items.Key;
-import fr.metro.game.exits.LockedExit;
 
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        String name;
+        if(args.length > 0){
+            name = args[0];
+            System.out.println("Username detected : "+name);
+        } else {
+            Scanner scanner = new Scanner(System.in);
 
-        System.out.print("What's your name ? ");
-        String name = scanner.nextLine();
-        System.out.println();
+            System.out.print("What's your name ? ");
+            name = scanner.nextLine();
+            System.out.println();
+        }
 
         Location loc2 = new Location("Salle 2");
         Exit exit = new Exit(loc2);
-        Map exits = new HashMap<String,Exit>();
+        Map<String, Exit> exits = new HashMap<>();
         exits.put("Sortie1", exit);
         Location loc1 = new Location(new ArrayList<>(), new ArrayList<>(), exits, "Salle 1");
 
