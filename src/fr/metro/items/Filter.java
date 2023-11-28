ae13a1a3ddd03bdf0c1132c4ea9a0920803eb20e
LeRoiDesKiwis <lekiwidu17@gmail.com>
2023-11-28 17:57:42 +0100

    replace string by stringbuilder

--- src/fr/metro/items/Filter.java
+++ src/fr/metro/items/Filter.java
@@ -1,30 +1,27 @@
 package fr.metro.items;
 
-import fr.metro.characters.GameCharacter;
-import fr.metro.game.Game;
-
 public class Filter extends Item {
     private static final int CAPACITY = 10;
-    private int capacityLeft ;
-    public Filter(){
+    private int capacityLeft;
+
+    public Filter() {
         super("Allows you to breath the irradiated air", ItemType.FILTER);
         capacityLeft = CAPACITY;
     }
 
-    public void useFilter(){
+    public void useFilter() {
         this.capacityLeft--;
     }
 
     @Override
-    public String toString(){
-        String s = super.toString() + ("\n     <!> filter status <!>\n");
-        s+=("[");
-        for (int i = 0; i<CAPACITY; i++){
-            if(i<capacityLeft){s+=(" * ");}
-            else {s+=(" - ");}
+    public String toString() {
+        StringBuilder s = new StringBuilder(super.toString() + ("\n     <!> filter status <!>\n"));
+        s.append("[");
+        for (int i = 0; i < CAPACITY; i++) {
+            s.append(i < capacityLeft ? " * " : " - ");
         }
-        s+=("]");
-        return s;
-    }
+        s.append("]");
+        return s.toString();
 
+    }
 }
