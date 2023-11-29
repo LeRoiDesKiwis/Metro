c9497cc6c71ba38dee55391a58493eee9a5af8a5
Fan2lasagne <150160586+Fan2lasagne@users.noreply.github.com>
2023-11-29 20:44:56 +0100

    Modified the init sequence

--- src/fr/metro/Main.java
+++ src/fr/metro/Main.java
@@ -3,12 +3,19 @@ package fr.metro;
 import fr.metro.characters.Inventory;
 import fr.metro.characters.Player;
 import fr.metro.characters.enemies.Enemy;
+import fr.metro.characters.friends.FriendlyCharacter;
+import fr.metro.characters.friends.SpeakingNPC;
 import fr.metro.game.Game;
 import fr.metro.game.Location;
 import fr.metro.game.exits.FinalExit;
 import fr.metro.game.exits.LockedExit;
 import fr.metro.items.Key;
+import fr.metro.items.foods.CannedFood;
+import fr.metro.items.foods.Mushroom;
 import fr.metro.items.weapons.Knife;
+import fr.metro.items.weapons.Pistol;
+import fr.metro.items.weapons.Riffle;
+import fr.metro.items.Filter;
 
 import java.util.List;
 import java.util.Scanner;
@@ -27,16 +34,17 @@ public class Main {
             System.out.println();
         }
 
-        Location location = new Location.LocationBuilder("Sokoi", "some description")
+        Location location = new Location.LocationBuilder("Sokol", "every story need a beginning")
                 .addItem(new Key())
-                .addCharacter(new Enemy("Robert", 5, new Inventory(List.of(new Knife()))))
-                .addExit(LockedExit.class, new Location.LocationBuilder("Dinamo")
-                        .addExit(new Location.LocationBuilder("Amino"))
-                        .addExit(new Location.LocationBuilder("Dostoievski")
-                                .addExit(new Location.LocationBuilder("Park Kultuty")
-                                        .addExit(LockedExit.class, new Location.LocationBuilder("Armory"))
+                .addCharacter(new Enemy("Piotr, lonely survivor", 5, new Inventory(List.of(new Knife()))))
+                .addExit(LockedExit.class, new Location.LocationBuilder("Dynamo", "an old metro station, looks clear")
+                        .addExit(new Location.LocationBuilder("Amino", "used to be crowded with mutated creatures").addItem(new Key()))
+                        .addExit(new Location.LocationBuilder("Dostoievski", "an old Red Line Reserve").addItem(new Filter()).addItem(new CannedFood()).addItem(new Mushroom())
+                                .addExit(new Location.LocationBuilder("Park Kultury", "used to be gorgeous now every last plant is rotten")
+                                        .addExit(LockedExit.class, new Location.LocationBuilder("Armory", "secret hideout, I hope no one is going to miss these weapons").addItem(new Riffle()).addItem(new Pistol()))
                                 )
-                                        .addExit(new Location.LocationBuilder("Polis")
+                                        .addExit(new Location.LocationBuilder("Polis", "finally some relief").addItem(new Filter()).addCharacter(new FriendlyCharacter("Anton", 10, new Inventory(), "Hello, friend") {
+                                                })
                                                 .addExit(FinalExit.class, new Location.LocationBuilder("final")))
                         )
                 ).build();
