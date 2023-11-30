cdda65e2760bd49e5c533a51f18b0fb69b55d556
PaulCB25 <76494231+PaulCB25@users.noreply.github.com>
2023-11-30 13:07:51 +0100

    added comments

--- src/fr/metro/characters/enemies/Enemy.java
+++ src/fr/metro/characters/enemies/Enemy.java
@@ -5,20 +5,22 @@ import fr.metro.characters.Inventory;
 import fr.metro.characters.Player;
 import fr.metro.items.Item;
 
+//Definition of our Enemy class, with a weapon
 public class Enemy extends GameCharacter {
-
     private String weaponName;
 
+    //Enemy constructor
     public Enemy(String name, int hp, Inventory inventory) {
         super(name, hp, inventory);
         inventory.getItemByType(Item.ItemType.WEAPON).ifPresent(item -> this.weaponName = item.name());
     }
 
+    //
     public void interact(Player player, String[] args){
         attack(player, weaponName);
     }
 
     public void talk(Player player){
-        System.out.println("I don't want to talk to you èwé !");
+        System.out.println("I don't want to talk to you !");
     }
 }
